48eefb5eae5412badf3ddc3d86bf9afb9f4468f9
Shin Haneul <s2018s08@e-mirim.hs.kr>
2020-05-19 19:20:13 +0900

    List와 Remove 예시

--- StudentList/Program.cs
+++ StudentList/Program.cs
@@ -23,6 +23,23 @@ namespace StudentList
             list.Add(new Teacher() { name = "김보경", major = "가정" });
             list.Add(new Teacher() { name = "장민주", major = "디자인" });
 
+            for (int i = list.Count -1; i >= 0; i++)
+            {
+                if (list[i].major == "정보컴퓨터")
+                {
+                    list.Remove(i);
+                }
+            }
+
+           /* foreach (var item in list)
+            {
+                if (item.major == "정보컴퓨터")
+                {
+                    list.Remove(item);
+                }
+                Console.WriteLine(item.name + " : " + item.major);
+            }*/
+
             foreach (var item in list)
             {
                 Console.WriteLine(item.name + " : " + item.major);
